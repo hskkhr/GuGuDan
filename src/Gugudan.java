9ec2573cf9d7cae7edfc854e163f7375d421e137
hskkhr <hskkhr@naver.com>
2022-01-03 12:50:08 +0900

    최종 미션 clear

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,14 +1,14 @@
 
 public class Gugudan {
-	public static int[] calc(int number) {
-		int[] arr = new int[9];
-		
+	public static int[] calc(int first,int second) {
+		int[] arr = new int[second];
 		for(int i=0;i<arr.length;i++) {
-			arr[i] = number * (i+1);
+			arr[i] = first * (i+1);
 		}
 		
 		return arr;
 	}
+
 	
 	public static void print(int[] result) {
 		for(int i=0;i<result.length;i++) {
--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -1,15 +1,37 @@
+import java.util.Scanner;
 
 public class GugudanMain {
 
 	public static void main(String[] args) {
-		//Gugudan ggd = new Gugudan();
 		
-		for(int i=2;i<10;i++) {
-			//ggd.print(ggd.calc(i));
-			System.out.println(i + "단");
-			Gugudan.print(Gugudan.calc(i));
-			System.out.println("");
+		Scanner sc = new Scanner(System.in);
+		String input_value = sc.nextLine();
+		int len = input_value.length();
+		
+		//최종 요구사항1
+		if(len==1) {
+			int first = Integer.parseInt(input_value);
+			
+			for(int i=2;i<first+1;i++) {
+				Gugudan.print(Gugudan.calc(i,first));
+			}
+			
 		}
+		//최종 요구사항2
+		else {
+			String[] splited_value = input_value.split(",");
+			int first = Integer.parseInt(splited_value[0]);
+			int second = Integer.parseInt(splited_value[1]);
+			
+			for(int i=2;i<first+1;i++) {
+				Gugudan.print(Gugudan.calc(i,second));
+			}
+
+		}
+		
+		
+		sc.close();
+		
 	}
 
 }
